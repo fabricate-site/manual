5f0396547ec4384d00bbbc5af7c99b101c9f8e26
Respatialized <respatialized@users.noreply.github.com>
2025-02-08 11:39:23 -0500

    Create flexible grid layout + begin adding defaults

--- docs/design/utopia.clj
+++ docs/design/utopia.clj
@@ -8,19 +8,20 @@
 
 
 ^{:kindly/kind :kind/hiccup}
-[:section {:class "u-grid-main"} [:h2 "Implementation checklist"]
+[:section {:class "u-grid-flex"} [:h2 "Implementation checklist"]
  [:ul #_{:style {:grid-column "1 / span 4"}} [:li "Import stylesheet"]
   [:li "Adjust grid to use " [:code "auto-fit"]]
   [:li "Set default styles for elements"] [:li "Begin defining utility classes"]
   [:li "Test out different spacing and size options"]]
  [:div
-  {:style {:border      "var(--space-xs) solid #222"
+  {:style {:border      "var(--space-xs) solid var(--color-brown)"
            :grid-column "span 4"
-           :height      "var(--space-3xl)"}}]]
-
-
-;; ideas:
-;; - the css after selector could be used to ensure that any grid item
-;; following
-;; a header could start on column 1 by default
-;;
+           :height      "var(--space-3xl)"}}]
+ [:div
+  {:style {:border      "var(--space-2xs) solid var(--color-yellow)"
+           :grid-column "1 / span 2"
+           :height      "var(--space-l)"}}]
+ [:div
+  {:style {:border      "var(--space-2xs) solid var(--color-green)"
+           :grid-column "span 2"
+           :height      "var(--space-l)"}}]]
